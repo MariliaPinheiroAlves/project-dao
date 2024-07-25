843d262f815325ce03b61743cf72a4b666ab8317
Marilia Pinheiro Alves <mariliapinheiroalves@gmail.com>
2024-07-25 20:15:08 -0300

    Add findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,11 +13,27 @@ public class Program {
 		SellerDao seller = DaoFactory.createSellerDao();
 		
 		Integer id = 10;
+		System.out.println("===== findById ====");
+		System.out.println("Id: " + id);
+		System.out.println("");
 		
 		if(seller.findByID(id) != null) {
 			System.out.println(seller.findByID(id));
 		} else {
 			System.out.println("404 - Not Found");			
+		}
+		
+		Department dp = new Department(2, null);
+		List<Seller> list = seller.findByDepartment(dp);
+		
+		System.out.println("");
+		System.out.println("===== findByDepartment ====");
+		System.out.println("Department: " + dp.getId());
+		System.out.println("");
+		
+		for(Seller s : list) {
+			System.out.println(s);	
+			System.out.println("");
 		}
 	}
 
