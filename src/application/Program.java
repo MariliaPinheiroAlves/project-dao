b73dd4546e83bc17525d8957ad7874e7fa99ab68
Marilia Pinheiro Alves <mariliapinheiroalves@gmail.com>
2024-07-25 20:50:08 -0300

    Add findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 		SellerDao seller = DaoFactory.createSellerDao();
 		
 		Integer id = 10;
-		System.out.println("===== findById ====");
+		System.out.println("===== findById =====");
 		System.out.println("Id: " + id);
 		System.out.println("");
 		
@@ -24,13 +24,23 @@ public class Program {
 		}
 		
 		Department dp = new Department(2, null);
-		List<Seller> list = seller.findByDepartment(dp);
+		List<Seller> listDepartment = seller.findByDepartment(dp);
 		
 		System.out.println("");
-		System.out.println("===== findByDepartment ====");
+		System.out.println("===== findByDepartment =====");
 		System.out.println("Department: " + dp.getId());
 		System.out.println("");
 		
+		for(Seller s : listDepartment) {
+			System.out.println(s);	
+			System.out.println("");
+		}
+		
+		System.out.println("");
+		System.out.println("===== findAll =====");
+		System.out.println("");
+		
+		List<Seller> list = seller.findAll();
 		for(Seller s : list) {
 			System.out.println(s);	
 			System.out.println("");
