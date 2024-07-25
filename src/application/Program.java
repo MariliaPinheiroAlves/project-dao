fbe3662c04bd2eb52597bddbb0548aa4a705da7a
Marilia Pinheiro Alves <mariliapinheiroalves@gmail.com>
2024-07-25 14:42:54 -0300

    Add class Department

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.sql.Connection;
+
+import db.DB;
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Connection conn = DB.getConnection();
+//		DB.closeConnection();
+		
+		Department dp = new Department(1, "Financeiro");
+		System.out.println(dp);
 
 	}
 
